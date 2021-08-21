6a15a46f5b994a6dca4378cae4f0a64371a76d4e
Muhammad Mufid <mufidu@outlook.com>
2021-08-21 21:45:51 +0700

    Make search works

--- src/script/components/verse-item.js
+++ src/script/components/verse-item.js
@@ -1,41 +1,50 @@
 /* eslint-disable accessor-pairs */
-class ClubItem extends HTMLElement {
+const axios = require('axios')
+
+class VerseItem extends HTMLElement {
   constructor () {
     super()
     this._shadowRoot = this.attachShadow({ mode: 'open' })
   }
 
-  set club (club) {
-    this._club = club
+  set verse (verse) {
+    return this.setter(verse)
+  }
+
+  async setter (verse) {
+    this._verse = verse
+    const arabText = await axios.get(`https://api.alquran.cloud/v1/ayah/${this._verse.number}`)
+    this._arabText = arabText.data.data.text
+    console.log(this._arabText)
     this.render()
   }
 
   render () {
     this._shadowRoot.innerHTML = `
             <style>
-            .club {
+            .verse {
                 margin-bottom: 18px;
                 box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
                 border-radius: 10px;
                 overflow: hidden;
             }
 
-            .club .fan-art-club {
+            .verse .fan-art-verse {
                 width: 100%;
                 max-height: 300px;
                 object-fit: cover;
                 object-position: center;
             }
 
-            .club-info {
+            .verse-info {
                 padding: 24px;
             }
 
-            .club-info > h2 {
+            .verse-info > h2 {
                 font-weight: lighter;
             }
 
-            .club-info > p {
+            .verse-info > p {
                 margin-top: 10px;
                 overflow: hidden;
                 text-overflow: ellipsis;
@@ -44,14 +53,15 @@ class ClubItem extends HTMLElement {
                 -webkit-line-clamp: 10; /* number of lines to show */
             }
             </style>
-            <div class="club">
-            <img class="fan-art-club" src="${this._club.strTeamBadge}" alt="Fan Art">
-                <div class="club-info">
-                <h2>${this._club.strTeam}</h2>
-                <p>${this._club.strDescriptionEN}</p></div>
+            <div class="verse">
+            <img class="fan-art-verse" src="${this._verse.number}" alt="Fan Art">
+                <div class="verse-info">
+                <h2>${this._arabText}</h2>
+                <p>${this._verse.text}</p>
+                <p>${this._verse.surah.englishName}:${this._verse.numberInSurah}</p></div>
             </div>
         `
   }
 }
 
-customElements.define('club-item', ClubItem)
+customElements.define('verse-item', VerseItem)
